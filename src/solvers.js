7f86b868a39b9c72266380ff98e3262e657d601f
Hack Reactor Student <student@hackreactor.com>
2017-05-25 16:43:41 -0700

    Work in progress: rooks recursion

--- src/solvers.js
+++ src/solvers.js
@@ -16,15 +16,54 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var newBoard = new Board({n: n});
+  // iterate through first row
+  for (var r = 0; r < n; r++) {
+    // iterate through first col
+    for (var c = 0; c < n; c++) { 
+      // set piece at row col 
+      newBoard.togglePiece(r, c);
+      // check if any conflicts for rooks
+      if (newBoard.hasAnyRooksConflicts()) {
+        // if conflict toggle piece
+        newBoard.togglePiece(r, c);
+      }
+    }
+  }
+  solution = newBoard;
+  
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var newBoard = new Board({n: n});
+  var solutionCount = 0;
+
+  let r = 0;
+  var recursion = function(r) {
+    for (var c = 0; c < newBoard.rows()[r].length; c++) { 
+      newBoard.togglePiece(r, c);
+      if (newBoard.hasAnyRooksConflicts()) {
+        newBoard.togglePiece(r, c);
+      } else {  // no conflict, recurse into branch
+
+      }
+    }
+    if (r === (n - 1)) {  // number of rows less than total rows available
+      solutionCount++;
+    }
+    if (r < (n - 1)) {  // number of rows less than total rows available
+      return recursion(r + 1);
+    }
+    
+    //solutionCount++; // where does solution count go?
+       
+  };
+  
+  recursion(r);
+
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
